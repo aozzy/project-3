b9054787deae724f1166f0b712edeb006527628b
aosman <pianogreen0@gmail.com>
2023-03-11 21:03:26 +0000

    vercel build

--- index.js
+++ index.js
@@ -1,58 +1,42 @@
-import path from 'path'
-const __dirname = path.resolve()
-const dist = path.join(__dirname, 'dist')
+import path from "path";
+const __dirname = path.resolve();
+const dist = path.join(__dirname, "dist");
 
-
-import express from 'express'
-import router from './views/router.js'
-import logger from './middleware/logger.js'
-import connectToDb from './lib/connectToDb.js'
-import errorHandler from './middleware/errorHandler.js'
+import express from "express";
+import router from "./views/router.js";
+import logger from "./middleware/logger.js";
+import connectToDb from "./lib/connectToDb.js";
+import errorHandler from "./middleware/errorHandler.js";
 // import { secret } from  './config/environment.js'
 
-import { PORT } from './config/environment.js'
-import dotenv from 'dotenv'
-dotenv.config()
-const app = express()
-
-
+import { PORT } from "./config/environment.js";
+import dotenv from "dotenv";
+dotenv.config();
+const app = express();
 
 async function startServer() {
+  await connectToDb();
 
-  await connectToDb()
+  console.log(" 🏙  Welcome to Cityscapes You are now connected to mongo!");
+  console.log(PORT);
 
-  console.log(' 🏙  Welcome to Cityscapes You are now connected to mongo!')
+  app.use(express.json());
 
+  app.use(logger);
 
+  app.use("/api", router);
 
-  app.use(express.json())
+  app.use(errorHandler);
 
-  app.use(logger)
+  app.use("/", express.static(dist));
 
-  app.use('/api', router)
+  app.get("*", function (req, res) {
+    res.sendFile(path.join(dist, "index.html"));
+  });
 
-  app.use(errorHandler)
-
-
-  app.use('/', express.static(dist))
-
-  app.get('*', function (req, res) {
-    res.sendFile(path.join(dist, 'index.html'))
-  })
-
-  app.listen(PORT, () => console.log(`🏙  Up and running on port ${PORT} `))
-
-
-
-
-
-
-
-
-
-
+  app.listen(PORT, () => console.log(`🏙  Up and running on port ${PORT} `));
 }
 
-startServer()
+startServer();
 
-export default app
+export default app;
